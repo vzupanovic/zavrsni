9a3aa097517a86ab0d0ba7f610fb39ccd13e721b
vz44975 <vanessa.zupanovic@fer.hr>
2012-06-24 16:51:43 +0200

    dodani komentari

--- simulator_v1.c
+++ simulator_v1.c
@@ -28,12 +28,12 @@
 
 KSEQ_INIT(gzFile, gzread);
 
-static double ERR_RATE = 0.02;
-static double MUT_RATE = 0.001;
-static double INDEL_FRAC = 0.15;
-static int GAP_SIZE = 1;
-static int SEED = -1;
-static double INDEL_EXT = 0.3;
+static double ERR_RATE = 0.02; //base calling error rate - default
+static double MUT_RATE = 0.001; //mutation rate - default
+static double INDEL_FRAC = 0.15; //rate of indels - default
+static int GAP_SIZE = 1;//size of gap - default
+static int SEED = -1;//seed for random number generator - default
+static double INDEL_EXT = 0.3;//probability that new random nuclotides are inserted into fragment - default
 
 typedef unsigned short mut_t;
 
@@ -57,9 +57,14 @@ gzFile fp,fpc;
  static uint64_t nG;
  static uint64_t nC;
  static uint64_t nN;
+
+ 
+/* ran_normal() - normal distribution approximation
+ * this function is copied from genran.c
+ * normally disturbed random number is returned*/
  
  
-double ran_normal() //normal distribution copied from generan.c
+double ran_normal()
 { 
     static int iset = 0; 
     static double gset; 
@@ -80,7 +85,14 @@ double ran_normal() //normal distribution copied from generan.c
     }
 }
 
-const int PoissonRandomNumber(const double lambda) //Poisson distribution
+
+/*poisson_random_number() - Poisson distribution approximation
+ * input parameter is lambda (number of base calling errors)
+ * number of iterations is set to 1000
+ * random number is returned*/
+
+
+const int poisson_random_number(const double lambda) //Poisson distribution
 {
   int k=0;                        
   const int max_k = 1000;           
@@ -89,19 +101,25 @@ const int PoissonRandomNumber(const double lambda) //Poisson distribution
   double sum=P;                     
   if (sum>=p) return 0;             
   for (k=1; k<max_k; ++k) {         
-    P*=lambda/(double)k;            
-    sum+=P;                         
-    if (sum>=p) break;              
+           P*=lambda/(double)k;            
+           sum+=P;                         
+           if (sum>=p) break;              
   }
-
   return k;                         
 }
 
 
-char swap_base(char base){ //uniform distribution
+/* swap_base() - changes the value of the base given as an input parameter
+ * value of new random base is calculated with uniform distribution 
+ * (function drand48()) the new value of the base is returned or
+ * the same base (if value of input parameter was N - unkown nucleotide)
+ * */
+
+
+char swap_base(char base){
 	char new_base;
 	double value;
-	value = drand48()*MAX;
+	value = drand48()*MAX; //value is number between 0 and 4
 	switch (base){
 		case 'A':
 		     if(value >= 1) return 'C';
@@ -125,9 +143,18 @@ char swap_base(char base){ //uniform distribution
 		     break;
 	    default:
 	         return base; 
-	   }
-	
+	   }	
 } 
+
+
+/* get_complement - calculates complement of the given parameter (nucleotide)
+ * A - T
+ * T - A
+ * G - C
+ * C - G
+ * returns complement or the input base if value of base was N - unkown nucleotide
+ * */
+
 
 char get_complement(char base){ //get complement A-T G-C
 	if (base == 'A') return 'T';
@@ -137,20 +164,36 @@ char get_complement(char base){ //get complement A-T G-C
 	else return base;
 }
 
-char *simulate_BCER(int read_length, char *read_local, int *n_errors){ //generates base calling errors using Poisson distribution 
+
+/*simulate_BCER() - simulates base calling errors, input parameters are:
+ * read_lenght - total length of the read
+ * read_local - read in which errors would be added
+ * n_errors - total number of errors calculated with poisson distribution
+ * returns read_local with errors added*/
+
+
+char *simulate_BCER(int read_length, char *read_local, int *n_errors){
 	double value;
 	int i,index;
-	*n_errors=(int)(PoissonRandomNumber((read_length*ERR_RATE)));
+	*n_errors=(int)(poisson_random_number((read_length*ERR_RATE))); //calculates total number of errors
 	for (i=0;i<*n_errors;i++){
-		value = drand48()*(read_length - 1); //calculate value of index (for mutation)
-		index = trunc((int)value);
+		value = drand48()*(read_length - 1);
+		index = trunc((int)value); //calculates index. the base on this index will be randomly changed
 		*(read_local+index) = swap_base(*(read_local+index)); //change base
     }
     return read_local;
-	
 }
 
-void generate_mutations(int dist, int size_l,int size_r){ //generates substitutions
+
+/*generate mutations() - generates mutations on input string, input parameters are:
+ * dist - distance between two reads
+ * size_l - size of first read
+ * size_r - size of second read
+ * function makes mutations on orginial string, so nothing is returned
+ * */
+  
+
+void generate_mutations(int dist, int size_l,int size_r){
 	int i,pos,n_mut;
 	double r;
 	n_mut = dist * MUT_RATE; //calculate total number of substitutions
@@ -159,16 +202,38 @@ void generate_mutations(int dist, int size_l,int size_r){ //generates substituti
 		pos = (int)(trunc(r * dist));
 		if(pos<=size_l){n_sub[0]++;} //calculate index for mutation
 		if(pos>=(dist-size_r)){n_sub[1]++;}
-		if(*(read_f+pos)!='N')
+		if(*(read_f+pos)!='N') //if value of base is N - skip!
 		    *(read_f+pos)=swap_base(*(read_f+pos));
 	}
 }
 
-void generate_gaps(int gap_pos, int gap_size){ //generates gaps by adding \0 character to string
+
+/*generate_gaps() - generates gaps on fragment by adding \0 character to it
+ * input parameters are:
+ * gap_pos - position of the gap
+ * gap size - size of gap (number of nucleotides that will be owerwritten)
+ * */
+
+
+void generate_gaps(int gap_pos, int gap_size){
 	uint64_t i;
 	read_f[gap_pos]='\0';
-	strcat(read_f,(read_f + gap_pos + gap_size));
+	strcat(read_f,(read_f + gap_pos + gap_size)); //overwrites gap_size nucleotides beginning from gap_pos 
 }
+
+
+/*core() - main function, calls every other function, if
+ * everything was fine during the simulation process returns 0 otherwise
+ * -1 , input parameters are:
+ * fout1 - file in which first fastq result of simulation for the first read will be stored
+ * fout2 - file in which second fastq result of simulation for the second read will be stored
+ * std_dev - standard deviation for fragments generation
+ * size_l - size of first read
+ * size_r - size of second read
+ * N - total number of reads
+ * dist - distance between two reads
+ * */
+
 
 int core(FILE *fout1,FILE *fout2,char *argv, int std_dev, int size_l, int size_r, uint64_t N,int dist){ //main function
 	mutseq_t *ret[2];
@@ -183,32 +248,39 @@ int core(FILE *fout1,FILE *fout2,char *argv, int std_dev, int size_l, int size_r
 	total_len = n_ref = 0;
 	frequency_A=frequency_T=frequency_G=frequency_C=0.;
 	nA=nT=nG=nC=j=0;
-	max_size = size_l > size_r? size_l : size_r;
+	max_size = size_l > size_r? size_l : size_r; //determine maximum size of the read
 	Q = (ERR_RATE == 0.0)? 'I' : (int)(-10.0 * log(ERR_RATE) / log(10.0) + 0.499) + 33; //calculates quality score
 	q_string = (char *)malloc((size_l+1)*sizeof(char)); //first quality string
 	q2_string = (char *)malloc((size_r+1)*sizeof(char)); // second quality string
-	for(int k=0;k<size_l;k++){q_string[k]=Q;};
+	for(int k=0;k<size_l;k++){q_string[k]=Q;};//prepare quality strings
 	for(int k=0;k<size_r;k++){q2_string[k]=Q;};
 	fprintf(stderr, "[%s] calculating the total length of the sequnce...\n",__func__);
-	while ((l = kseq_read(seq)) >= 0){ //prints out basic info and calculates total length of sequence
-		printf("[%s] name: %s\n",__func__,seq->name.s);
-		if(seq->comment.l) printf("[%s] comment: %s\n",__func__,seq->comment.s);
-		total_len+=l;
-		++n_ref;
-		if (seq->qual.l) printf ("qual: %s\n",seq->qual.s);
+	
+	while ((l = kseq_read(seq)) >= 0){ //prints out basic info and calculates the total length of sequence
+		     printf("[%s] name: %s\n",__func__,seq->name.s);
+		     if(seq->comment.l) printf("[%s] comment: %s\n",__func__,seq->comment.s);
+		     total_len+=l;
+		     ++n_ref;
+		     if (seq->qual.l) printf ("qual: %s\n",seq->qual.s);
 	}
+	
+	if (total_len == 0){ //in case of empty file simulation is terminated, -1 is returned
+		     printf("[%s] input file is empty!\n",__func__);
+		     return -1;
+	}
+	
 	fprintf(stderr, "[%s] %d sequences, total length: %llu\n", __func__, n_ref, (long long)total_len);
 	kseq_destroy(seq);
-	
 	gzclose(fp);
 	fp = gzopen(argv, "r");
 	seq = kseq_init(fp);
+	
 	while ((l = kseq_read(seq)) >= 0){
 		if (l < dist + 3*std_dev){
 			fprintf(stderr,"[%s] ERROR sequence to short for given parametars!\n",__func__); //check if sequence too short for given parameters
 			return -1;
 		}
-		for (i=0;i<l;i++){
+		for (i=0;i<l;i++){ //calculate the total number of A,T,G,C
 		if(seq->seq.s[i]== 'A')
 		     nA++;
 		else if(seq->seq.s[i] == 'T')
@@ -226,34 +298,37 @@ int core(FILE *fout1,FILE *fout2,char *argv, int std_dev, int size_l, int size_r
 	frequency_G = (double_t)nG/l;
 	frequency_T = (double_t)nT/l;
 	frequency_N = (double_t)nN/l;
+	
 	j++;	  
     printf("[%s] frequency per sequence [%llu/%llu] A - %f | C - %f | G - %f | T - %f | *N - %f - unknown nucleotides (percentage) \n",__func__,(long long)j,(long long)n_ref,frequency_A*100,frequency_C*100,frequency_G*100,frequency_T*100,frequency_N*100);
     }
-    tot_seq = (char *)malloc((total_len+1)*sizeof(char)); //allocates memory space for sequence
+    tot_seq = (char *)malloc((total_len+1)*sizeof(char)); //allocates enough memory space for sequence
 	tot_seq = seq->seq.s;
 	printf("[%s] transferring sequence into memory and generating errors...\n",__func__);
-	counter_a=counter_b=0;
+	counter_a=counter_b=0; //set counters to zero
 	for(i=0;i<N;i++){
 		double ran;
 		int d,pos;
 		uint64_t fp1_b,fp1_e,fp2_b,fp2_e,begin,end;
 		char *read1,*read2;
-		n_sub[0]=n_sub[1]=n_err[0]=n_err[1]=n_ind[0]=n_ind[1]=0;
+		n_sub[0]=n_sub[1]=n_err[0]=n_err[1]=n_ind[0]=n_ind[1]=0; //set the number of substitutions, errors and indels to zero (this would be printed in header of the output files)
+		
 		do{
 			ran = ran_normal();
 			ran = ran * std_dev + dist;
-			d = (int)(ran + 0.5);
-			d = d > max_size ? d : max_size;
-			pos = (int)((total_len-d+1)*drand48()); //calculates the position (first index) of read with normal distribution
+			d = (int)(ran + 0.5); //calculates size of fragment
+			d = d > max_size ? d : max_size; //avoid boundary failure
+			pos = (int)((total_len-d+1)*drand48()); //calculates the position (first index) of fragment with normal distribution
 		}while(pos < 0 || pos >= total_len || (pos + d - 1) >= total_len);
-		read_f = (char *)malloc((d+1+(int)((INDEL_FRAC+0.5)*d))*sizeof(char));counter_a++; //allocates enough memory for the read
-		read_f[d+1]='\0';
-		strncpy(read_f,tot_seq+pos,d);
-		begin=pos; end=pos+d;
+		
+		read_f = (char *)malloc((d+1+(int)((INDEL_FRAC+0.5)*d))*sizeof(char));counter_a++; //allocates enough memory for the fragment
+		read_f[d+1]='\0'; //adds \0 to the end of the fragment
+		strncpy(read_f,tot_seq+pos,d); //copies part of the sequence into fragment
+		begin=pos; end=pos+d; //store beginning
 		generate_mutations(d,size_l,size_r); //generates mutations
 		int n_n=0;
-		int n_indel = (int)(INDEL_FRAC * d);
-		int curr_dist=d;
+		int n_indel = (int)(INDEL_FRAC * d); //total number of indels
+		int curr_dist=d; //distance between two reads
 		if((n_indel >= d) || ((GAP_SIZE*n_indel)>=d)){
 			fprintf(stderr,"[%s] ERROR sequence too short for given parametars!\n",__func__); //checks if sequence is too short for given parameters
 			return -1;
@@ -261,25 +336,25 @@ int core(FILE *fout1,FILE *fout2,char *argv, int std_dev, int size_l, int size_r
 		do{
 			int type_indel,gap_size;
 			char *fragment;
-			type_indel = (drand48()>=INDEL_EXT)?1:0;
+			type_indel = (drand48()>=INDEL_EXT)?1:0; //if 0 generates gaps otherwise adds new random nucleotides
 			if (type_indel == 1){
 				pos = (int)trunc(drand48()*(curr_dist-1)); //calculates the position of gap
 				if (pos<size_l) n_ind[0]++;
 				if (pos >= (d-size_r)) n_ind[1]++;
-				gap_size = PoissonRandomNumber(GAP_SIZE); //generates gaps
+				gap_size = poisson_random_number(GAP_SIZE); //generates gaps
 				if (gap_size){
 			        generate_gaps(pos,gap_size);
 				}
-			    curr_dist=curr_dist-gap_size;
+			    curr_dist=curr_dist-gap_size; //reduce size of fragment
 		    }
 			else{
-				char fragment[4]={0x0};
-				char keeper[1000]={0x0};
+				char fragment[4]={0x0}; //new nucleotides will be stored here
+				char keeper[1000]={0x0}; //temporary string
 				int pos;
 				double r;
 				char base;
 				r = drand48();
-				base=(r < 0.25)?'A':((r>=0.25 && r<0.5)?'T':(r>=0.25 && r<0.75)?'C':'G');
+				base=(r < 0.25)?'A':((r>=0.25 && r<0.5)?'T':(r>=0.25 && r<0.75)?'C':'G'); //generates new random base
 				fragment[0]=base;fragment[1]='\0'; //creates new random nucleotide
 				pos = (int)trunc(drand48()*(curr_dist-1)); //calculates the position for insertion
 				if (pos<size_l) n_ind[0]++;
@@ -294,12 +369,12 @@ int core(FILE *fout1,FILE *fout2,char *argv, int std_dev, int size_l, int size_r
 			n_n++;
 		}while(n_n<n_indel);
 		read1=(char *)malloc((size_l+1)*sizeof(char)); //generates two pair end reads 
-		n_errors=(int)(INDEL_FRAC*size_l);
+		n_errors=(int)(INDEL_FRAC*size_l); //calculates the total number of base calling errors
 		read2=(char *)malloc((size_r+1)*sizeof(char));
 		strncpy(read1,read_f,size_l);read1[size_l+1]='\0';
 		int internal_counter=0;
 		for(int k=(curr_dist-1);internal_counter<size_r;k--){
-			*(read2+internal_counter)=get_complement(*(read_f + k)); //makes complement of first fragment
+			*(read2+internal_counter)=get_complement(*(read_f + k)); //makes complement of first fread
 			internal_counter++;
 		}
 		read2[internal_counter]='\0';
@@ -320,6 +395,10 @@ int core(FILE *fout1,FILE *fout2,char *argv, int std_dev, int size_l, int size_r
 	free(q2_string);
 }
 
+
+/* simu_usage() - displays menu on standard error*/
+
+
 static int simu_usage(){
 	fprintf(stderr,"**********************************************************\n");
 	fprintf(stderr,"Program: simulator (short read simulator)\n");
@@ -339,6 +418,8 @@ static int simu_usage(){
 	fprintf(stderr,"\n**********************************************************\n");
 	return 1;
 }
+
+
 
 int main(int argc, char *argv[])
 {
